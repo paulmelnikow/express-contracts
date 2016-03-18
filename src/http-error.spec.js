50516270709d9241e8c9dc4f994d4298395361b0
Quinn Maurmann <quinn.maurmann@gmail.com>
2016-03-18 15:21:14 -0400

    Tweak HttpError test

--- src/http-error.spec.js
+++ src/http-error.spec.js
@@ -3,9 +3,12 @@ var errors = require('./http-error');
 describe('Tests for HttpError', function () {
 
     it('should serialize in accordance with contract', function (done) {
-        var e = new errors.HttpError('Validation error', 400);
-        errors.httpErrorContract.check(e.toJSON());
-
+        var e = new errors.HttpError('Validation error', 400),
+            doc = e.toJSON();
+        errors.httpErrorContract.check(doc);
+        doc.error.should.equal(true);
+        doc.message.should.equal('Validation error');
+        doc.httpStatus.should.equal(400);
         done();
     });
 
