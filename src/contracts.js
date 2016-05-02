ca8c9bd341847d77594e61db464f296d33ce4402
Paul Melnikow <paul.melnikow@bodylabs.com>
2016-05-02 15:05:22 -0400

    Formatting

--- src/contracts.js
+++ src/contracts.js
@@ -1,8 +1,5 @@
 var c = require('rho-contracts');
 
-module.exports = {
-    // Optional simple format for errors, used by
-    // `middleware.useContractsOrError`.
-    errorBody: c.object({ error: c.string }).strict()
-        .rename('errorBody'),
-};
+// Optional simple format for errors, used by `middleware.useContractsOrError`.
+module.exports.errorBody = c.object({ error: c.string }).strict()
+    .rename('errorBody');
