f87790fd7bc965dc83e3393daa3d84e32dcbd11b
Paul Melnikow <paul.melnikow@bodylabs.com>
2016-05-02 15:35:46 -0400

    Respond to PR comments and clean this up a bit

--- src/middleware.js
+++ src/middleware.js
@@ -1,18 +1,16 @@
-var impl = require('./middleware.impl'),
-    c = require('rho-contracts');
+var c = require('rho-contracts');
 
 var cc = {};
 
 cc.middleware = c.any.rename('middleware');
 
-cc.useContracts = c.fun({ requestContract: c.contract }, { responseBodyContract: c.contract })
-    .returns(cc.middleware);
+var middlewareContracts = {
+    useContracts = c.fun({ requestContract: c.contract }, { responseBodyContract: c.contract })
+        .returns(cc.middleware),
 
-// Reduce boilerplate for controllers of having to union with contracts.errorBody
-cc.useContractsOrError = c.fun({ requestContract: c.contract }, { responseBodyContract: c.contract })
-    .returns(cc.middleware);
-
-module.exports = {
-    useContracts: cc.useContracts.wrap(impl.useContracts),
-    useContractsOrError: cc.useContracts.wrap(impl.useContractsOrError),
+    // Reduce boilerplate for controllers of having to union with contracts.errorBody
+    useContractsOrError = c.fun({ requestContract: c.contract }, { responseBodyContract: c.contract })
+        .returns(cc.middleware),
 };
+
+module.exports = middlewareContracts.wrap(require('./middleware.impl'));
